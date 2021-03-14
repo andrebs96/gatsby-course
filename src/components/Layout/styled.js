7094b47036c3a132f4b5abc12fbe8d164ec6cb03
Andre Beserra <andresouza@multconnect.com.br>
2021-03-14 12:56:41 -0300

    #Secao10

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -5,7 +5,13 @@ display: flex;
 `
 
 export const LayoutMain = styled.main`
-min-height: 100vh;
-padding: 0 3.75rem 0 20rem;
-width: 100%;
+    min-height: 100vh;
+    padding: 0 3.75rem 0 20rem;
+    width: 100%;
+
+    body#grid & {
+        grid-template-areas:
+        "posts"
+        "pagination";
+    }
 `
--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -6,6 +6,8 @@ import SEO from "../components/seo"
 import PostItem from '../components/PostItem'
 import Pagination from '../components/Pagination'
 
+import * as S from '../components/ListWrapper/styled'
+
 const BlogList = props => {    
 
     const postList = props.data.allMarkdownRemark.edges
@@ -19,24 +21,26 @@ const BlogList = props => {
     return(
         <Layout>
             <SEO title="Home" />
-            {postList.map(({
-                node: {
-                frontmatter: { background, category, date, description, title },
-                timeToRead,
-                fields: { slug }
-                },
-            }, index) => (
-                <PostItem 
-                    key={index}
-                    slug={slug}
-                    background={background}
-                    category={category}
-                    date={date}
-                    timeToRead={timeToRead}
-                    title={title}
-                    description={description}
-                />
-            ))}
+            <S.ListWrapper>
+                {postList.map(({
+                    node: {
+                    frontmatter: { background, category, date, description, title },
+                    timeToRead,
+                    fields: { slug }
+                    },
+                }, index) => (
+                    <PostItem 
+                        key={index}
+                        slug={slug}
+                        background={background}
+                        category={category}
+                        date={date}
+                        timeToRead={timeToRead}
+                        title={title}
+                        description={description}
+                    />
+                ))}
+            </S.ListWrapper>
             <Pagination 
                 isFirst={isFirst}
                 isLast={isLast}
