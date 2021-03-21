ea15291ccc902a276d012741abc4821c34b66386
Andre Beserra <andresouza@multconnect.com.br>
2021-03-20 21:20:37 -0300

    #Secao12

--- gatsby-node.js
+++ gatsby-node.js
@@ -47,6 +47,7 @@ exports.createPages = ({ graphql, actions }) => {
               date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
               title
               description
+              image
             }
             timeToRead
             wordCount {
